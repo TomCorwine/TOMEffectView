5a45a889c000af4b557296424d508e822bb4bfff
Tom Corwine <tc@corwine.org>
2014-02-22 00:47:49 -0500

    Lots of copying and pasting.

--- TOMEffectView/TOMEffectView.h
+++ TOMEffectView/TOMEffectView.h
@@ -6,9 +6,20 @@
 //  Copyright (c) 2014 Tom Corwine. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Availability.h>
 
-@interface TOMEffectView : UIView
+#ifndef __IPHONE_5_0
+#	error This library uses features only available in iOS SDK 5.0 and later.
+#endif
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_5_0
+#	error This library requires devices to be running iOS 5.0 or later
+#endif
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface TOMEffectView : GLKView
 
 @property (nonatomic) CGFloat blur;
 
--- TOMEffectView/TOMEffectView.m
+++ TOMEffectView/TOMEffectView.m
@@ -8,6 +8,352 @@
 
 #import "TOMEffectView.h"
 
+// Uniform index.
+enum
+{
+    UNIFORM_Y,
+    UNIFORM_UV,
+    NUM_UNIFORMS
+};
+GLint uniforms[NUM_UNIFORMS];
+
+// Attribute index.
+enum
+{
+    ATTRIB_VERTEX,
+    ATTRIB_TEXCOORD,
+    NUM_ATTRIBUTES
+};
+
+@interface TOMEffectView ()
+{
+	GLuint _program;
+    
+    GLuint _positionVBO;
+    GLuint _texcoordVBO;
+    GLuint _indexVBO;
+	
+	CGFloat _screenWidth;
+    CGFloat _screenHeight;
+    size_t _textureWidth;
+    size_t _textureHeight;
+    unsigned int _meshFactor;
+    
+    EAGLContext *_context;
+    
+    CVOpenGLESTextureRef _lumaTexture;
+    CVOpenGLESTextureRef _chromaTexture;
+	CVOpenGLESTextureCacheRef _videoTextureCache;
+}
+
+@end
+
 @implementation TOMEffectView
+
++ (Class)layerClass
+{
+	return [CAEAGLLayer class];
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+	self = [super initWithFrame:frame];
+
+	// From Apple sample code https://developer.apple.com/library/ios/samplecode/GLCameraRipple/Introduction/Intro.html#//apple_ref/doc/uid/DTS40011222-Intro-DontLinkElementID_2
+	
+	EAGLContext *context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+	if (nil == context)
+	{
+		NSLog(@"Unable to initialize EAGL context.");
+		return nil;
+	}
+	if (NO == [EAGLContext setCurrentContext:context])
+	{
+		NSLog(@"Unable to set EAGL context.");
+		return nil;
+	}
+	
+	self.context = context;
+
+	[self setupGL];
+
+	return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+	CVImageBufferRef pixelBuffer = NULL;
+	glActiveTexture(GL_TEXTURE0);
+    CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+                                                       _videoTextureCache,
+                                                       pixelBuffer,
+                                                       NULL,
+                                                       GL_TEXTURE_2D,
+                                                       GL_RED_EXT,
+                                                       _textureWidth,
+                                                       _textureHeight,
+                                                       GL_RED_EXT,
+                                                       GL_UNSIGNED_BYTE,
+                                                       0,
+                                                       &_lumaTexture);
+    if (err)
+    {
+        NSLog(@"Error at CVOpenGLESTextureCacheCreateTextureFromImage %d", err);
+    }
+    
+    glBindTexture(CVOpenGLESTextureGetTarget(_lumaTexture), CVOpenGLESTextureGetName(_lumaTexture));
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+    // UV-plane
+    glActiveTexture(GL_TEXTURE1);
+    err = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+                                                       _videoTextureCache,
+                                                       pixelBuffer,
+                                                       NULL,
+                                                       GL_TEXTURE_2D,
+                                                       GL_RG_EXT,
+                                                       _textureWidth/2,
+                                                       _textureHeight/2,
+                                                       GL_RG_EXT,
+                                                       GL_UNSIGNED_BYTE,
+                                                       1,
+                                                       &_chromaTexture);
+    if (err)
+    {
+        NSLog(@"Error at CVOpenGLESTextureCacheCreateTextureFromImage %d", err);
+    }
+    
+    glBindTexture(CVOpenGLESTextureGetTarget(_chromaTexture), CVOpenGLESTextureGetName(_chromaTexture));
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+}
+
+#pragma mark - Accessors
+
+- (void)setAlpha:(CGFloat)alpha
+{
+	super.alpha = alpha;
+}
+
+- (void)setHidden:(BOOL)hidden
+{
+	super.hidden = hidden;
+}
+
+#pragma mark - OpenGL ES 2
+
+- (void)setupGL
+{
+    //[EAGLContext setCurrentContext:_context];
+    
+    [self loadShaders];
+    
+    glUseProgram(_program);
+	
+    glUniform1i(uniforms[UNIFORM_Y], 0);
+    glUniform1i(uniforms[UNIFORM_UV], 1);
+}
+
+- (void)tearDownGL
+{
+    //[EAGLContext setCurrentContext:_context];
+    
+    glDeleteBuffers(1, &_positionVBO);
+    glDeleteBuffers(1, &_texcoordVBO);
+    glDeleteBuffers(1, &_indexVBO);
+    
+    if (_program) {
+        glDeleteProgram(_program);
+        _program = 0;
+    }
+}
+
+- (BOOL)loadShaders
+{
+    GLuint vertShader, fragShader;
+    
+    // Create shader program.
+    _program = glCreateProgram();
+	
+	NSString *vShader = @"\
+	attribute vec4 position;\
+	attribute vec4 inputTextureCoordinate;\
+	\
+	varying vec2 textureCoordinate;\
+	\
+	void main()\
+	{\
+		gl_Position = position;\
+		textureCoordinate = inputTextureCoordinate.xy;\
+	}";
+	
+	NSString *fShader = @"\
+	varying highp vec2 textureCoordinate;\
+	uniform sampler2D inputImageTexture;\
+	\
+	void main()\
+	{\
+		gl_FragColor = texture2D(inputImageTexture, textureCoordinate);\
+	}";
+    
+    // Create and compile vertex shader.
+    if (![self compileShader:&vertShader type:GL_VERTEX_SHADER string:vShader]) {
+        NSLog(@"Failed to compile vertex shader");
+        return NO;
+    }
+    
+    // Create and compile fragment shader.
+    if (![self compileShader:&fragShader type:GL_FRAGMENT_SHADER string:fShader]) {
+        NSLog(@"Failed to compile fragment shader");
+        return NO;
+    }
+    
+    // Attach vertex shader to program.
+    glAttachShader(_program, vertShader);
+    
+    // Attach fragment shader to program.
+    glAttachShader(_program, fragShader);
+    
+    // Bind attribute locations.
+    // This needs to be done prior to linking.
+    glBindAttribLocation(_program, ATTRIB_VERTEX, "position");
+    glBindAttribLocation(_program, ATTRIB_TEXCOORD, "inputTextureCoordinate");
+    
+    // Link program.
+    if (![self linkProgram:_program]) {
+        NSLog(@"Failed to link program: %d", _program);
+        
+        if (vertShader) {
+            glDeleteShader(vertShader);
+            vertShader = 0;
+        }
+        if (fragShader) {
+            glDeleteShader(fragShader);
+            fragShader = 0;
+        }
+        if (_program) {
+            glDeleteProgram(_program);
+            _program = 0;
+        }
+        
+        return NO;
+    }
+    
+    // Get uniform locations.
+    uniforms[UNIFORM_Y] = glGetUniformLocation(_program, "SamplerY");
+    uniforms[UNIFORM_UV] = glGetUniformLocation(_program, "SamplerUV");
+    
+    // Release vertex and fragment shaders.
+    if (vertShader) {
+        glDetachShader(_program, vertShader);
+        glDeleteShader(vertShader);
+    }
+    if (fragShader) {
+        glDetachShader(_program, fragShader);
+        glDeleteShader(fragShader);
+    }
+    
+    return YES;
+}
+
+- (BOOL)compileShader:(GLuint *)shader type:(GLenum)type string:(NSString *)string
+{
+    GLint status;
+    const GLchar *source = (GLchar *)string.UTF8String;
+    
+    if (!source) {
+        NSLog(@"Failed to load vertex shader");
+        return NO;
+    }
+    
+    *shader = glCreateShader(type);
+    glShaderSource(*shader, 1, &source, NULL);
+    glCompileShader(*shader);
+    
+#ifdef DEBUG
+    GLint logLength;
+    glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &logLength);
+    if (logLength > 0) {
+        GLchar *log = (GLchar *)malloc(logLength);
+        glGetShaderInfoLog(*shader, logLength, &logLength, log);
+        NSLog(@"Shader compile log:\n%s", log);
+        free(log);
+    }
+#endif
+    
+    glGetShaderiv(*shader, GL_COMPILE_STATUS, &status);
+    if (status == 0) {
+        glDeleteShader(*shader);
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (BOOL)linkProgram:(GLuint)prog
+{
+    GLint status;
+    glLinkProgram(prog);
+    
+#ifdef DEBUG
+    GLint logLength;
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &logLength);
+    if (logLength > 0) {
+        GLchar *log = (GLchar *)malloc(logLength);
+        glGetProgramInfoLog(prog, logLength, &logLength, log);
+        NSLog(@"Program link log:\n%s", log);
+        free(log);
+    }
+#endif
+    
+    glGetProgramiv(prog, GL_LINK_STATUS, &status);
+    if (status == 0) {
+        return NO;
+    }
+    
+    return YES;
+}
+
+#pragma mark - 
+
+- (void)cleanUpTextures
+{
+    if (_lumaTexture)
+    {
+        CFRelease(_lumaTexture);
+        _lumaTexture = NULL;
+    }
+    
+    if (_chromaTexture)
+    {
+        CFRelease(_chromaTexture);
+        _chromaTexture = NULL;
+    }
+    
+    // Periodic texture cache flush every frame
+    CVOpenGLESTextureCacheFlush(_videoTextureCache, 0);
+}
+
+- (void)setupBuffers
+{
+    glGenBuffers(1, &_indexVBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexVBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, [_ripple getIndexSize], [_ripple getIndices], GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &_positionVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, _positionVBO);
+    //glBufferData(GL_ARRAY_BUFFER, [_ripple getVertexSize], [_ripple getVertices], GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(ATTRIB_VERTEX);
+    glVertexAttribPointer(ATTRIB_VERTEX, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), 0);
+	
+    glGenBuffers(1, &_texcoordVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, _texcoordVBO);
+    //glBufferData(GL_ARRAY_BUFFER, [_ripple getVertexSize], [_ripple getTexCoords], GL_DYNAMIC_DRAW);
+    
+    glEnableVertexAttribArray(ATTRIB_TEXCOORD);
+    glVertexAttribPointer(ATTRIB_TEXCOORD, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), 0);
+}
 
 @end
